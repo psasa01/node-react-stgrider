c98a38700c399641737e8f4517f46356928b5acf
unknown <psasa@gmail.com.com>
2018-03-23 12:27:44 +0100

    google/fb

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({'hi': 'there'})
-})
+    res.send({
+        hi: 'there'
+    })
+} )
+
+passport.use(new GoogleStrategy(
+    {
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, accessToken => {
+    console.log(accessToken);
+}));
+
+passport.use(new FacebookStrategy({
+    clientID: keys.FACEBOOK_APP_ID, 
+    clientSecret: keys.FACEBOOK_APP_SECRET,
+    callbackURL: "/auth/facebook/callback"
+}, accessToken => {
+    console.log(accessToken);
+}));
+
+app.get(
+    '/auth/google', 
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+}));
+
+app.get(
+    '/auth/facebook',
+    passport.authenticate('facebook'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT, () => {
+    console.log('server started on port ' + PORT)
+});
