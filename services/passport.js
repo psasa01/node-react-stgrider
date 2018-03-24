66997029b34ac65fd4e0a9ad7a9b780785feff14
unknown <psasa@gmail.com.com>
2018-03-24 12:58:34 +0100

    environment

--- services/passport.js
+++ services/passport.js
@@ -68,7 +68,7 @@ passport.use(new FacebookStrategy({
     clientID: keys.FACEBOOK_APP_ID,
     clientSecret: keys.FACEBOOK_APP_SECRET,
     callbackURL: 'https://shrouded-coast-13620.herokuapp.com/auth/facebook/callback',
-    profileFields: ['email']
+    profileFields: ['id', 'emails', 'name']
 }, (accessToken, refreshToken, profile, done) => {
        process.nextTick(() => {
            console.log(profile);
