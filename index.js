4ab1f56bfeeb56ddc62194c1b54cb4866d11e1fa
unknown <psasa@gmail.com.com>
2018-03-29 15:35:20 +0200

    ready for heroku

--- index.js
+++ index.js
@@ -50,6 +50,19 @@ app.get('/user', (req, res) => {
 
 require('./routes/authRoutes')(app);
 
+if(process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    // main.js, main.css
+    app.use(express.static('client/build'));
+
+    // Express will serve up index.html 
+    // if it doesn't recognize the route!
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log('server started on port ' + PORT)
