b0e4c8228add692ce19cd5684281a93931879218
unknown <psasa@gmail.com.com>
2018-04-13 15:49:19 +0200

    130418

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import formFields from './formFields';
-import _ from 'lodash'
+import _ from 'lodash';
+import * as actions from '../../actions';
 
 
-const SurveyFormReview = ({ onCancel, formValues }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
 
     const reviewFields = _.map(formFields, ({ name, label }) => {
         return (
@@ -22,10 +24,18 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
             <h5>Please confirm your entries</h5>
             {reviewFields}
             <button
-                className="btn-flat darken-3 yellow"
+                className="btn-flat darken-3 yellow white-text"
                 onClick={onCancel}
             >
                 Cancel
+                <i className="material-icons right">cancel</i>
+            </button>
+            <button 
+                className="btn-flat green white-text right"
+                onClick={() => submitSurvey(formValues, history)}
+            >
+                Submit Survey 
+                <i className="material-icons right">email</i>
             </button>
         </div>
     )
@@ -38,4 +48,4 @@ const mapStateToProps = (state) => {
     };
 }
 
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview));
